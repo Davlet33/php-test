38cac428dd1b6d6171020fd0dcf49964bb13e0a1
DavletMain <ali_magomedov_2000@inbox.ru>
2024-02-28 18:10:37 +0300

    first commit

---
+++ Patterns/Singleton.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class Singleton
+{
+    private static self $instance;
+
+    private function __construct()
+    {
+    }
+
+    public static function getInstance(): self
+    {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+}
+
+$instance = Singleton::getInstance();
---
+++ index.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+abstract class Worker
+{
+    public function working()
+    {
+        return print 'тут';
+    }
+}
+
+
+class Work extends Worker
+{
+    public function __construct()
+    {
+    }
+
+    public function calculate(int $a, int $b): int
+    {
+        return $a + $b;
+    }
+
+    public function work(int $a, int $b): array
+    {
+        return [$this->calculate($a, $b), $this->working()];
+    }
+}
+
+$w = new Work();
+
+var_dump($w->work(2, 3));
+//$w->work();
